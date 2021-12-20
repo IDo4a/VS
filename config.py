c4142e0d4e613265ae10665018a2ed30cb44230d
Romanchyk Artem <73314321+IDo4a@users.noreply.github.com>
2021-12-20 15:25:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-open_weather_token = "6e9401955e7b8e6c37f16242bc7d396d"
-tg_bot_weather = "5087410624:AAHXtdilA3wu2oHlaoPiG524bdOp_MBlLOQ"
-tg_bot_exchange = "5051026997:AAH2Jj9JIh4-LBamOJO46CxWk4Lzy8DthtI"
+open_weather_token = "Your_token"
+tg_bot_weather = "Your_token"
+tg_bot_exchange = "Your_token"
 
 
